c603ae4f7642534b223d8afac85021d90632d5eb
Gopi <gopiariv@gmail.com>
2020-09-06 23:48:56 +0530

    created TodoController

---
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -0,0 +1,14 @@
+package com.example.todo.controller;
+
+import com.example.todo.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TodoController {
+
+    @Autowired
+    TodoRepository todoRepository;
+}
