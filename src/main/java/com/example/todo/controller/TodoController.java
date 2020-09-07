c393a83939db60b7719f0eb4372835e005da10c7
Gopi <gopiariv@gmail.com>
2020-09-07 21:42:17 +0530

    added a get api for retrieving all todos

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -1,9 +1,15 @@
 package com.example.todo.controller;
 
+import com.example.todo.model.Todo;
 import com.example.todo.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -11,4 +17,19 @@ public class TodoController {
 
     @Autowired
     TodoRepository todoRepository;
+
+    public ResponseEntity<List<Todo>> getAllTodos() {
+        try {
+            List<Todo> todos = new ArrayList<Todo>();
+            todoRepository.findAll().forEach(todos::add);
+
+            if (todos.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(todos, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
 }
