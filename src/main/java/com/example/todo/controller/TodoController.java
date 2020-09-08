8ea92afcd2826b5dd57a331234c0ec3919235453
Gopi <gopiariv@gmail.com>
2020-09-08 12:46:40 +0530

    added the crud apis

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -5,11 +5,11 @@ import com.example.todo.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -18,6 +18,7 @@ public class TodoController {
     @Autowired
     TodoRepository todoRepository;
 
+    @GetMapping("/todos")
     public ResponseEntity<List<Todo>> getAllTodos() {
         try {
             List<Todo> todos = new ArrayList<Todo>();
@@ -32,4 +33,77 @@ public class TodoController {
         }
 
     }
+
+    @GetMapping("/todo/{id}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable("id") String id) {
+        Optional<Todo> todoData = todoRepository.findById(id);
+        if (todoData.isPresent()) {
+            return new ResponseEntity<>(todoData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @PostMapping("/todos")
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo todo) {
+        try {
+            Todo _todo = todoRepository.save(new Todo(todo.getTitle(), todo.getDescription(), false));
+            return new ResponseEntity<>(_todo, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable("id") String id, @RequestBody Todo todo) {
+        Optional<Todo> todoData = todoRepository.findById(id);
+        if (todoData.isPresent()) {
+            Todo _todo = todoData.get();
+            _todo.setTitle(todo.getTitle());
+            _todo.setDescription(todo.getDescription());
+            _todo.setCompleted(todo.isCompleted());
+
+            return new ResponseEntity<>(todoRepository.save(_todo), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public ResponseEntity<HttpStatus> deleteTodo(@PathVariable("id") String id) {
+        try {
+            todoRepository.deleteById(id);
+
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @DeleteMapping("/todos")
+    public ResponseEntity<HttpStatus> deleteAllTodo() {
+        try {
+            todoRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<List<Todo>> findActiveTodos() {
+        try {
+            List<Todo> activeTodosList = todoRepository.findByCompleted(false);
+
+            if (activeTodosList.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(activeTodosList, HttpStatus.OK);
+        }  catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 }
