c8aecc2e88919d16aa5a9b3dd1371470ecbc747f
Gopi <gopiariv@gmail.com>
2020-09-14 00:00:12 +0530

    added error files

---
+++ src/main/java/com/example/todo/exception/ControllerExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.example.todo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    public ResponseEntity<ErrorMessage> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        ErrorMessage errorMessage = new ErrorMessage(
+                HttpStatus.NOT_FOUND.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+    }
+
+    public ResponseEntity<ErrorMessage> globalExceptionHandler(ResourceNotFoundException ex, WebRequest request) {
+        ErrorMessage errorMessage = new ErrorMessage(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
