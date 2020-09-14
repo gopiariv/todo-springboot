cd7585a92feacb8097c49b9ed462dc6004238c26
Gopi <gopiariv@gmail.com>
2020-09-14 18:25:06 +0530

    configured database

--- src/main/java/com/example/todo/model/Todo.java
+++ src/main/java/com/example/todo/model/Todo.java
@@ -1,7 +1,12 @@
 package com.example.todo.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "todos")
 public class Todo {
 
+    @Id
     private String id;
 
     private String title;
